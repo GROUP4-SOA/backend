86f7e4568906c10ec2b45e760790809696ac81a9
thienhuong320 <phantranthienhuong320@gmail.com>
2025-03-28 23:22:48 +0700

    update entities

--- Bookstore-Inventory/Bookstore.Domain/Entities/Book.cs
+++ Bookstore-Inventory/Bookstore.Domain/Entities/Book.cs
@@ -11,7 +11,8 @@ namespace Bookstore.Domain.Entities
         public int BookId { get; set; }
         public string Title { get; set; }
         public string Author { get; set; }
-        public string ISBN { get; set; }
+        public decimal Price { get; set; }
+        public DateTime Isbn { get; set; }
         public int CategoryId { get; set; }
         public Category Category { get; set; }
         public int Quantity { get; set; }
--- Bookstore-Inventory/Bookstore.Infrastructure/obj/Release/net8.0/Bookstore.Infrastructure.AssemblyInfo.cs
+++ Bookstore-Inventory/Bookstore.Infrastructure/obj/Release/net8.0/Bookstore.Infrastructure.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("Bookstore.Infrastructure")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+49d11ccb36e4f2aaeb955eadfe2efe61e86b71e2")]
 [assembly: System.Reflection.AssemblyProductAttribute("Bookstore.Infrastructure")]
