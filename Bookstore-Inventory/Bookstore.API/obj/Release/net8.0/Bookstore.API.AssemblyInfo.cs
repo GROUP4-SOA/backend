d49162151fa49acfdc20291e64eab57292d0ed08
hnhngc <huynhngc176@gmail.com>
2025-03-29 02:22:43 +0700

    Change Readonly

--- Bookstore-Inventory/Bookstore.API/obj/Release/net8.0/Bookstore.API.AssemblyInfo.cs
+++ Bookstore-Inventory/Bookstore.API/obj/Release/net8.0/Bookstore.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Bookstore.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ed42e9b5ddd6d125c53855b1df99665247a270a1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+86f7e4568906c10ec2b45e760790809696ac81a9")]
 [assembly: System.Reflection.AssemblyProductAttribute("Bookstore.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Bookstore.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Bookstore-Inventory/Bookstore.Infrastructure/obj/Release/net8.0/Bookstore.Infrastructure.AssemblyInfo.cs
+++ Bookstore-Inventory/Bookstore.Infrastructure/obj/Release/net8.0/Bookstore.Infrastructure.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Bookstore.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+49d11ccb36e4f2aaeb955eadfe2efe61e86b71e2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+86f7e4568906c10ec2b45e760790809696ac81a9")]
 [assembly: System.Reflection.AssemblyProductAttribute("Bookstore.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Bookstore.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
