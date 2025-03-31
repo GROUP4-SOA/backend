97f560ea080e1766bc4eafab37b5c1d1cfa85dca
hnhngc <huynhngc176@gmail.com>
2025-03-31 12:36:23 +0700

    abt

--- Bookstore-Inventory/Bookstore.Domain/obj/Release/net8.0/Bookstore.Domain.AssemblyInfo.cs
+++ Bookstore-Inventory/Bookstore.Domain/obj/Release/net8.0/Bookstore.Domain.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Bookstore.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ae40db153ace70e1f4aa662a1b50587abfa7a9d2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bf362c01915cec0334f3bd4ec5a79ffe2176b4fe")]
 [assembly: System.Reflection.AssemblyProductAttribute("Bookstore.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Bookstore.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
