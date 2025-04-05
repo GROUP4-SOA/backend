5b26c805fe137d782b3c39412a3dc2decdbd0fce
LemyTruc <trucle.31221026452@st.ueh.edu.vn>
2025-04-05 18:05:01 +0700

    test coverage

--- Bookstore-Inventory/Bookstore.Tests/obj/Debug/net9.0/Bookstore.Tests.AssemblyInfo.cs
+++ Bookstore-Inventory/Bookstore.Tests/obj/Debug/net9.0/Bookstore.Tests.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Bookstore.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+03e42111d152b9f9a7a8b5fe0f1a02323bbb30f7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e052a9798648ef5ee856ee025ff406427af6b95b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Bookstore.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Bookstore.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
